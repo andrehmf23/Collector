da4824056e4ca92cce8483bb40ebca25be0037a9
andrehmf23 <andrehmf23@gmail.com>
2025-10-22 20:22:45 -0300

    clear and add Camera page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_application_collector/pages/item_page.dart';
 import 'package:flutter_application_collector/pages/items_page.dart';
 import 'package:flutter_application_collector/pages/register_page.dart';
 
